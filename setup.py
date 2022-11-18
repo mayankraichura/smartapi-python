856456481f192a756317c12aff40f599a0314065
mayank.raichura <mayank.raichura@gmail.com>
2022-11-18 10:37:01 +0530

    Change version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
     ]
 
 setup(
-    name="msr-smartapi-python",
+    name="SmartApi",
     version="1.3.8",
     author="ab-smartapi",
     author_email="smartapi.sdk@gmail.com",
